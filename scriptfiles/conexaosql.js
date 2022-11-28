aabff3dcd1874718b8adec9e6960d47ff5bdab1e
Henrique <hfrutter@gmail.com>
2022-11-28 19:31:45 -0300

    SQL cria o banco

--- scriptfiles/conexaosql.js
+++ scriptfiles/conexaosql.js
@@ -9,4 +9,11 @@ var con = mysql.createConnection({
 con.connect(function (erroCon){
     if(erroCon) throw erroCon;
     console.log("Conectado ao banco de dados!");
+    con.query("CREATE DATABASE loja", function (erroCon, result) {
+        if(erroCon) throw erroCon;
+        console.log("Banco de dados criado com sucesso.");
+        if(erroCon) throw erroCon;
+        console.log("O banco de dados já existe, continuando");
+
+    })
 });
