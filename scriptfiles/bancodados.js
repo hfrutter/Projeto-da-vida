33a55f7fef628898bf6a7c63c040b728c98f1d1d
Henrique <hfrutter@gmail.com>
2022-11-29 14:41:22 -0300

    sqlinserindodados

--- scriptfiles/bancodados.js
+++ scriptfiles/bancodados.js
@@ -1,30 +1,24 @@
 var mysql = require('mysql')
 
-var con = mysql.createConnection({
+var con = mysql.createConnection(
+{
     host: "localhost",
     user: "root",
     password: "",
     database: "loja",
 });
 
-con.connect(function (erroCon){
-    if(erroCon) throw erroCon;
+con.connect(function(erroCon) 
+{
+    if (erroCon) throw erroCon;
     console.log("Conectado ao banco de dados!");
-    if(erroCon) throw (erroCon);
-
-    function adicionarCliente(id, nome, compras, telefone, endereco) {
-        id = 1;
-        nome = "Henrique Rutter";
-        compras = 1;
-        telefone = 11932242324;
-        endereco = "Rua da independencia 784";
-    
-    
-        var sql = "INSERT INTO clientes (id, nome, compras, telefone, endereco), VALUES, id, nome, compras, telefone, endereco"; 
-        if(erroCon) throw erroCon;
-        console.log("Finalizado com sucesso!");
-    
-    }
-
+    var sql = "INSERT INTO clientes (id, nome, endereco, compras, telefone) VALUES ('', 'Antonio','R. Fidalga, 209-1 - Pinheiros','2','1123456789')";
+    con.query(sql, function (erroCon, result) 
+    {
+      if (erroCon) throw erroCon;
+      console.log("Dados inseridos com sucesso!");
+    });
 });
+
+
 
--- scriptfiles/iniciarsql.js
+++ scriptfiles/iniciarsql.js
@@ -7,18 +7,16 @@ var con = mysql.createConnection({
     database: "loja",
 });
 
-con.connect(function (erroCon){
-    if(erroCon) throw erroCon;
-    console.log("Conectado ao banco de dados!");
-    if(erroCon) throw (erroCon);
+    // Não funciona ainda, a pretenção é pegar os arquivos csv e já puxar deles os dados e jogar no banco de dados, mas como são apenas 4 de cada estou fazendo manualmente.
+    //function adicionarCliente(id, nome, compras, telefone, endereco)
+    
 
-    //var sql = "CREATE TABLE clientes (nome VARCHAR(255), compras INT, telefone INT, endereco VARCHAR(255))";
-    //var sql = "DROP TABLE clientes"
-    var sql = "CREATE TABLE clientes (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255), compras INT, telefone INT, endereco VARCHAR(255))";
-    con.query(sql, function (erroCon, result) {
-        if(erroCon) throw erroCon;
-        console.log(erroCon);
-        console.log("Tabela criada com sucesso!");
-    })
-
-})
+    con.connect(function(erroCon) {
+        if (erroCon) throw erroCon;
+        console.log("Conectado ao Banco de dados!");
+        var sql = "CREATE TABLE clientes (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255), endereco VARCHAR(255), compras INT, telefone VARCHAR(255))";
+        con.query(sql, function (erroCon, result) {
+          if (erroCon) throw erroCon;
+          console.log("Tabela criada com sucesso");
+        });
+      });
