4ae82a236aee1b31623698d464d83f88ccba138d
Henrique <hfrutter@gmail.com>
2022-11-30 07:43:38 -0300

    29112020

--- scriptfiles/aio.js
+++ scriptfiles/aio.js
@@ -1,25 +1,27 @@
 var mysql = require('mysql')
-
 var banco = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "loja",
 });
-
+var resultado;
 banco.connect(function(erroCon) 
 {
     if (erroCon) throw erroCon;
     console.log("Conectado ao banco de dados!");
-    var sql = "INSERT INTO clientes (id, nome, endereco, compras, telefone) VALUES ('', 'Antonio','R. Fidalga, 209-1 - Pinheiros','2','1123456789')";
+    /*var sql = "INSERT INTO clientes (id, nome, endereco, compras, telefone) VALUES ('', 'Antonio','R. Fidalga, 209-1 - Pinheiros','2','1123456789')";
     banco.query(sql, function (erroCon, result) 
     {
       if (erroCon) throw erroCon;
       console.log("Dados inseridos com sucesso!");
     });
+    */
     banco.query("SELECT * FROM clientes", function (erroCon, result, fields) 
     {
         if(erroCon) throw erroCon;
         console.log(result);
+        resultado = result;
+        
     });
 });
--- scriptfiles/bancodados.js
+++ scriptfiles/bancodados.js
@@ -11,12 +11,13 @@ banco.connect(function(erroCon)
 {
     if (erroCon) throw erroCon;
     console.log("Conectado ao banco de dados!");
-    var sql = "INSERT INTO clientes (id, nome, endereco, compras, telefone) VALUES ('', 'Antonio','R. Fidalga, 209-1 - Pinheiros','2','1123456789')";
-    banco.query(sql, function (erroCon, result) 
+    //var sql = "INSERT INTO clientes (id, nome, endereco, compras, telefone) VALUES ('', 'Antonio','R. Fidalga, 209-1 - Pinheiros','2','1123456789')";
+    /*banco.query(sql, function (erroCon, result) 
     {
       if (erroCon) throw erroCon;
       console.log("Dados inseridos com sucesso!");
     });
+    */
     banco.query("SELECT * FROM clientes", function (erroCon, result, fields) 
     {
         if(erroCon) throw erroCon;
